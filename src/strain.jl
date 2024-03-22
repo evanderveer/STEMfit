aff36766b8a1f441926e3731e2dc7cae8eaf2b6c
Ewout van der Veer <ewoutvanderveer@msn.com>
2024-03-22 16:48:15 +0100

    update strain.jl

--- src/strain.jl
+++ src/strain.jl
@@ -6,45 +6,56 @@ Description:
 Functions for calculating the local strain from atomic positions. 
 """
 
+@enum LAYER_DIRECTION y x
+
 """
     function get_strain_from_lattice_parameters(
-        lattice_parameters::AbstractMatrix{T},
+        results::Results,
         bulk_lattice_parameter_dict::Dict{<:Integer, <:Tuple},
-        layer_assignments::AbstractVector{<:Integer}    
+        layer_boundaries::AbstractVector{<:Integer},
+        layer_direction::LAYER_DIRECTION = y    
     ) where {T<:Real} -> Matrix{T}
 
-    Calculates the strain from the local lattice parameters values in `lattice_parameters`,
-    the bulk lattice_parameters `bulk_lattice_parameter_dict` and layer assignments. 
+    Calculates the true strain from the local lattice parameters values in `results`,
+    the bulk lattice_parameters `bulk_lattice_parameter_dict` and layer boundaries. 
     `bulk_lattice_parameter_dict` is a dictionary whose keys correspond to the layer indices
-    in `layer_assignments` and whose values are tuples of bulk lattice parameters in the same
-    directions as the two rows of `lattice_parameters`.
+    starting from the top/left of the image. The optional `layer_direction` can be either
+    `x` if the layers are vertical or `y` if they are horizontal (default). 
+
+    The strain calculated using this function is the *true* strain in the film relative
+    to the bulk lattice parameter of the material of each film, so not the same as the strain
+    as it is calculated using techniques like geometric phase analysis, where the strain is 
+    relative to a single set of lattice parameters (generally of the substrate). For this,
+    use the local lattice parameter. 
 """
-function get_strain_from_lattice_parameters(#TODO: Make it work with Results struct
-    lattice_parameters::AbstractMatrix{T},
+function calculate_true_strain(
+    results::Results,
     bulk_lattice_parameter_dict::Dict{<:Integer, <:Tuple},
-    layer_assignments::AbstractVector{<:Integer}    
-    ) where {T<:Real}
-    if length(layer_assignments) != size(lattice_parameters)[2]
-        throw(ArgumentError("lattice_parameters and layer_assignment must have the same size"))
+    layer_boundaries::AbstractVector{<:Integer},
+    layer_direction::LAYER_DIRECTION = y    
+    ) 
+    #Make sure we have the right number of layers
+    if length(layer_boundaries) != length(bulk_lattice_parameter_dict) - 1
+        throw(ArgumentError("wrong number of layer boundaries provided"))
     end
 
-    #Make sure all layers have their parameters in the dict
-    layer_set = (keys(bulk_lattice_parameter_dict)..., 0)
-    if !all((x -> x ∈ layer_set).(Set(layer_assignments)))
-        throw(ArgumentError("bulk lattice parameters of some layers were not provided"))
-    end
+    layer_numbers = layer_assignments(
+                                        results.atom_parameters, 
+                                        layer_boundaries,
+                                        layer_direction
+                                        )
 
-    strain_matrix = similar(lattice_parameters)
-    for (i, col) in enumerate(eachcol(lattice_parameters))
-        if layer_assignments[i] == 0
-            strain_matrix[:, i] = zeros(T, 2)
+    results.strain = similar(results.lattice_parameters)
+    for (i, col) in enumerate(eachcol(results.lattice_parameters))
+        if layer_numbers[i] == 0
+            results.strain[:, i] = zeros(T, 2)
             continue
         end
 
-        strain = col ./ bulk_lattice_parameter_dict[layer_assignments[i]] .- 1
-        strain_matrix[:, i] = strain
+        strain = col ./ bulk_lattice_parameter_dict[layer_numbers[i]] .- 1
+        results.strain[:, i] = strain
     end
-    strain_matrix
+    
 end
 
 """
@@ -57,25 +68,27 @@ end
     the `layer_boundaries`. Assumes that layer boundaries are horizontal. The values
     in `layer_boundaries` are the y-values of the boundaries in the image.
 """
-function layer_assignments(#TODO: Make it work with Results struct
+function layer_assignments(
     atom_parameters::AtomParameters{T}, 
-    layer_boundaries::AbstractVector;
-    plot::Bool = true
+    layer_boundaries::AbstractVector,
+    layer_direction::LAYER_DIRECTION
     ) where T
 
     atom_positions = atom_parameters.centroids
 
-    layer_boundaries = [zero(T), T.(layer_boundaries)..., maximum(atom_positions[1, :])]
+    #Are the layers horizontal or vertical?
+    row = layer_direction == y ? 1 : 2
+
+    layer_boundaries = [zero(T), T.(layer_boundaries)..., maximum(atom_positions[row, :])]
     layer_filters = []
     for boundary in eachindex(layer_boundaries[2:end])
         push!(layer_filters, layer_boundaries[boundary] .<
-                             atom_positions[1, :] .<= 
-                             layer_boundaries[boundary+1]) 
+                             atom_positions[row, :] .<= 
+                             layer_boundaries[boundary + 1]) 
     end
-    layer_assignments = sum(filter .* i for (i,filter) in enumerate(layer_filters), dims=2)
 
-    if plot
-        map_layer_assignment(atom_positions, layer_assignments)
-    end
+    #Give each layer a new index incrementally
+    layer_assignments = sum(filter .* i for (i, filter) in enumerate(layer_filters), dims=2)
+
     layer_assignments
 end
