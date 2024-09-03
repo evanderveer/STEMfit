5ac7c0b89e4f8389c35a7d6bf9a391f218e33894
Ewout van der Veer <ewoutvanderveer@msn.com>
2024-09-03 16:07:19 +0200

    save Results struct to file

--- src/fileio.jl
+++ src/fileio.jl
@@ -7,75 +7,104 @@ Functions for loading and saving results.
     
 """
 
-function save_atomic_parameters_mat(
+function save_results_nm(
+    results::Results,
+)
+    headers = ["y0 (nm)", "x0 (nm)", "A", "length (nm)", "width (nm)", "angle (deg)"]
+    data_matrix = [headers atom_parameters_to_matrix(results.atom_parameters, results.pixel_sizes)]
+
+    lattice_parameters_nm = similar(results.lattice_parameters)
+    for i in axes(lattice_parameters_nm, 2)
+        lattice_parameters_nm[:, i] = results.lattice_parameters[:, i] .* results.pixel_sizes
+    end
+    if results.lattice_parameters !== nothing
+        data_matrix = [data_matrix; ["vector 1 lp (nm)", "vector 2 lp (nm)"] lattice_parameters_nm]
+    end
+end
+
+function save_results_px(
+    results::Results,
+)
+    headers = ["y0 (px)", "x0 (px)", "A", "length (px)", "width (px)", "angle (deg)"]
+    data_matrix = [headers atom_parameters_to_matrix(results.atom_parameters)]
+
+    if results.lattice_parameters !== nothing
+        data_matrix = [data_matrix; ["vector 1 lp (px)", "vector 2 lp (px)"] results.lattice_parameters]
+    end
+end
+
+function save_results(
     filename::String,
-    atom_parameters::AbstractMatrix{<:Real},
-    lattice_parameters::Union{AbstractMatrix{<:Real}, Nothing} = nothing,
-    strain::Union{AbstractMatrix{<:Real}, Nothing} = nothing,
-    valid_atoms::Union{AbstractVector{Bool}, Nothing} = nothing
+    results::Results,
     )
-    if all([atom_parameters, lattice_parameters, strain, valid_atoms] .=== nothing)
-        throw(ArgumentError("no data was provided"))
+    
+    check_results_valid(results)
+
+    if isnothing(results.pixel_sizes)
+        data_matrix = save_results_px(results)
+    else
+        data_matrix = save_results_nm(results)
     end
 
-    sizes = [size(i, 2) for i in [atom_parameters, lattice_parameters, strain] 
-                                if i !== nothing]
-    size_set = valid_atoms === nothing ? Set(sizes) : Set([sizes..., length(valid_atoms)])
-
-    if length(size_set) != 1
-        throw(ArgumentError("all parameters must have the same length"))
+    if results.strain !== nothing
+        data_matrix = [data_matrix; ["vector 1 strain", "vector 2 strain"] results.strain]
     end
 
-    if size(atom_parameters, 1) == 6
-        data_matrix = [["y0", "x0", "A", "length", "width", "angle"] atom_parameters]
-    elseif size(atom_parameters, 1) == 2
-        data_matrix = [["y0", "x0"] atom_parameters]
-    end
-
-    if lattice_parameters !== nothing
-        data_matrix = [data_matrix; ["vector 1 lp", "vector 2 lp"] lattice_parameters]
-    end
-
-    if strain !== nothing
-        data_matrix = [data_matrix; ["vector 1 strain", "vector 2 strain"] strain]
-    end
-
-    if valid_atoms !== nothing
-        data_matrix = data_matrix[:, [true; valid_atoms]]
+    if results.valid_atoms !== nothing
+        data_matrix = data_matrix[:, [true; results.valid_atoms]]
     end
 
     writedlm(filename, permutedims(data_matrix, (2,1)), ',')
 end
 
-function save_atomic_parameters(
-    filename::String,
-    atom_parameters::AtomParameters,
-    lattice_parameters::Union{AbstractMatrix{<:Real}, Nothing} = nothing,
-    strain::Union{AbstractMatrix{<:Real}, Nothing} = nothing,
-    valid_atoms::Union{AbstractVector{Bool}, Nothing} = nothing
-    )
-    atom_parameters_matrix = [atom_parameters.centroids; 
-                       atom_parameters.intensities';
-                       atom_parameters.sizes;
-                       atom_parameters.angles']
-    save_atomic_parameters_mat(filename,
-                           atom_parameters_matrix,
-                           lattice_parameters,
-                           strain,
-                           valid_atoms)
+function check_results_valid(results::Results)
+    sizes = [size(i, 2) for i in [
+                                  results.atom_parameters.centroids, 
+                                  results.lattice_parameters, 
+                                  results.strain
+                                 ] 
+        if i !== nothing]
+    size_set = results.valid_atoms === nothing ? Set(sizes) : Set([sizes..., length(results.valid_atoms)])
+    if length(size_set) != 1
+        throw(ArgumentError("all parameters must have the same length"))
+    end
 end
 
-function save_atomic_parameters(
-    filename::String,
-    results::Results,
+function atom_parameters_to_matrix(ap::AtomParameters)
+    [ap.centroids; 
+     ap.intensities';
+     ap.sizes;
+     ap.angles']
+end
 
+function atom_parameters_to_matrix(
+    ap::AtomParameters, 
+    px_size::Tuple{Real, Real}
     )
-    
-    save_atomic_parameters(filename,
-                           results.atom_parameters,
-                           results.lattice_parameters,
-                           results.strain,
-                           results.valid_atoms)
+    ap_centroids_nm = similar(ap.centroids)
+    for i in axes(ap_centroids_nm, 2)
+        ap_centroids_nm[:, i] = ap.centroids[:, i] .* px_size
+    end
+
+    ap_sizes_nm = similar(ap.sizes)
+    for i in axes(ap_sizes_nm, 2)
+        px_size_rot = get_rotated_pixel_size(px_size, ap.angles[i])
+        ap_sizes_nm[:, i] = ap.sizes[:, i] .* px_size_rot
+    end
+    [ap_centroids_nm; 
+     ap.intensities';
+     ap_sizes_nm;
+     ap.angles']
+end
+
+function get_rotated_pixel_size(
+    px_size::Tuple{Real, Real}, 
+    angle::AbstractFloat
+)
+    (
+        sqrt((px_size[1]*cosd.(-angle))^2 + (px_size[2]*sind.(-angle))^2),
+        sqrt((px_size[1]*cosd.(90+angle))^2 + (px_size[2]*sind.(90+angle))^2)
+    )
 end
 
 function load_atomic_parameters(
